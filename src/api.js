f3236ff4c2aa8471a7815c322dd0d6fae7ca44bb
Aqeel Adil <aqeel.adil17@gmail.com>
2025-04-05 05:35:47 +0500

    env variable update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://34.226.121.126/api/";
+const API_URL = import.meta.env.VITE_API_URL;;
 
 // User Registration
 export const registerUser = async (userData) => {
